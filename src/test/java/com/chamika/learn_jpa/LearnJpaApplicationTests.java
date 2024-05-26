d43baec5fb0f8a4ba3ad2df8655e6bd203a61135
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-05-26 11:30:19 +0530

    commit  with docker setup

---
+++ src/test/java/com/chamika/learn_jpa/LearnJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamika.learn_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
