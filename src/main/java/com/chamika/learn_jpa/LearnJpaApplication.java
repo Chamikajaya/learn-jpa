a9265ac97726d1e890a07a99ed7fa637630d3c48
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-05-27 06:47:34 +0530

    revised the concepts learned so far

--- src/main/java/com/chamika/learn_jpa/LearnJpaApplication.java
+++ src/main/java/com/chamika/learn_jpa/LearnJpaApplication.java
@@ -25,33 +25,39 @@ public class LearnJpaApplication {
     CommandLineRunner runner(StudentRepository studentRepository, StudentIdCardRepository studentIdCardRepository) {
         return args -> {
 
-//            Faker faker = new Faker();
-//            Random random = new Random();
-//
-//            for (int i = 0; i < 60; i++) {
-//                var name = faker.name();
-//                String firstName = name.firstName();
-//                String lastName = name.lastName();
-//                String fullName = firstName + " " + lastName;
-//                String email = fullName.split(" ")[0].toLowerCase() + "@gmail.com";
-//                Integer age = random.nextInt(18, 100);
-//
-//                Student student = new Student(firstName, lastName, email, age);
-//
-//                studentRepository.save(student);
-//            Student student = new Student("Chamika", "Jayasinghe", "chamika@gmail.com", 22);
-//            StudentIdCard studentIdCard = new StudentIdCard("123456", student);
-//
-//            studentIdCardRepository.save(studentIdCard);  // ! This will save the student and the student id card as well.
+            Faker faker = new Faker();
+            Random random = new Random();
+
+            for (int i = 0; i < 50; i++) {
+                var name = faker.name();
+                String firstName = name.firstName();
+                String lastName = name.lastName();
+                String fullName = firstName + " " + lastName;
+                String email = fullName.split(" ")[0].toLowerCase() + "@gmail.com";
+                Integer age = random.nextInt(18, 100);
+
+                Student student = new Student(firstName, lastName, email, age);
+
+                String idCardNumber = faker.number().digits(6); // Generates a random 6-digit number
+                studentIdCardRepository.save(new StudentIdCard(idCardNumber, student));  // ! This will save the student and the student id card as well. 👈 First Saving the Student and then the Student Id Card will not work due to transactional issues.
+            }
+
+            // *  Revising Pagination and Sorting
+
+            PageRequest pageRequest = PageRequest.of(
+                    0,
+                    10,
+                    Sort.by("age").descending().and(Sort.by("firstName").ascending())
+            );
+
+            Page<Student> studentPage = studentRepository.findAll(pageRequest);
+
+
+            studentPage.forEach((student) -> System.out.println(student.getFirstName() + " " + student.getLastName() + " --> " + student.getAge()));
 
 
 
-            // Since we have a bi-directional relationship between the student and the student id card, if we load the student, we can also load the student id card and vice versa. 👈
-//            studentIdCardRepository.findById(1).ifPresent(System.out::println);
-//            System.out.println("---------------------------------------------------");
-//            studentRepository.findById(61).ifPresent(System.out::println);
 
-            studentRepository.deleteStudentById(61);
 
         };
     }
